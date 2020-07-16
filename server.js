707d4b3405a6901c9e71996941095762d693c3cd
jeffdrodrigues <67475105+jeffdrodrigues@users.noreply.github.com>
2020-07-16 20:40:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const express = require('express');
+
+const app = express();
+
 app.use(express.static('./dist/angular-nestjs'));
 
-app.get('/*', função(req, res) { 
-  res.sendFile('index.html', {root: 'dist/angular-nestjs/'} 
-); 
-});
-app.listen(processo.env.PORT || 8080);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-nestjs/'}),
+);
+
+app.listen(process.env.PORT || 8080);
